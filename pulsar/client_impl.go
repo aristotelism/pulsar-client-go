912f377a80f2a6d047df372dbff86ad6ae4e6b1b
Aristotelis Mertis <aristotelis.mertis@citrix.com>
2022-12-08 12:45:53 +0200

    moved auth package

--- pulsar/client_impl.go
+++ pulsar/client_impl.go
@@ -26,7 +26,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/apache/pulsar-client-go/pulsar/internal"
-	"github.com/apache/pulsar-client-go/pulsar/internal/auth"
+	"github.com/apache/pulsar-client-go/pulsar/auth"
 	"github.com/apache/pulsar-client-go/pulsar/log"
 )
 
